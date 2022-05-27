c03062eee60613fa5f9626973a24269f1e125e54
RamzRjs <60762747+RameesaRijas@users.noreply.github.com>
2022-05-27 11:48:19 -0400

    building binary tree from inorder and postorder

---
+++ binaryTree1.js
@@ -0,0 +1,50 @@
+// Given two integer arrays inorder and postorder where inorder is the inorder traversal of a binary tree and postorder is the postorder traversal of the same tree, construct and return the binary tree.
+
+// Example 1:
+
+
+// Input: inorder = [9,3,15,20,7], postorder = [9,15,7,20,3]
+// Output: [3,9,20,null,null,15,7]
+// Example 2:
+
+// Input: inorder = [-1], postorder = [-1]
+// Output: [-1]
+ 
+
+// Constraints:
+
+// 1 <= inorder.length <= 3000
+// postorder.length == inorder.length
+// -3000 <= inorder[i], postorder[i] <= 3000
+// inorder and postorder consist of unique values.
+// Each value of postorder also appears in inorder.
+// inorder is guaranteed to be the inorder traversal of the tree.
+// postorder is guaranteed to be the postorder traversal of the tree.
+
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {number[]} inorder
+ * @param {number[]} postorder
+ * @return {TreeNode}
+ */
+const buildTree = (inorder, postorder) => {
+
+  //for post order root will be last element
+  const root = new TreeNode(postorder[postorder.length - 1]);
+  const mid = inorder.indexOf(postorder.pop());
+
+  //getting the left
+  root.left = buildTree(inorder.slice(0, mid), postorder.slice(0, mid))
+  //getting right
+  root.right = buildTree(inorder.slice(mid + 1, inorder.length), postorder.slice(mid, postorder.length))
+
+  return root;
+}
